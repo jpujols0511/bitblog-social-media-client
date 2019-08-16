15aaa1c4db80ef9c55c6fc4b159b689f25967a39
Joan Pujols <joanpujols1998@gmail.com>
2019-08-16 08:42:10 -0400

    Implementing more profile features like changing profile picture and biography of the user

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles'
+import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
-
+import dayjs from 'dayjs';
+import EditDetails from './EditDetails';
 //MUI stuff
-
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
-
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip'
 //Icons
 import LocationOn from '@material-ui/icons/LocationOn';
+import LinkIcon from '@material-ui/icons/Link';
+import CalendarToday from '@material-ui/icons/CalendarToday';
+import EditIcon from '@material-ui/icons/Edit';
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
 
 //Redux
 import { connect } from 'react-redux';
+import { logoutUser, uploadImage } from '../redux/actions/userActions'
 
 
 
-const styles = {
+const styles = (theme) => ({
 
   paper: {
     padding: 20
@@ -64,23 +71,48 @@ const styles = {
       margin: '20px 10px'
     }
   }
-};
+});
 
 class Profile extends Component {
+  handleImageChange = (event) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    this.props.uploadImage(formData);
+  };
+  handleEditPicture = () => {
+    const fileInput = document.getElementById('imageInput');
+    fileInput.click();
+  };
+  handleLogout = () => {
+    this.props.logoutUser();
+  }
   render() {
     const { 
       classes, 
       user: { 
         credentials: {handle, createdAt, imageUrl, bio, website, location}, 
-        loading
+        loading,
+        authenticated
       }
       } = this.props;
 
     let profileMarkup = !loading ? (authenticated ? (
       <Paper className={classes.paper}>
         <div className={classes.profile}>
-          <div className="profile-image">
-              <img src={imageUrl} alt="Profile" />
+          <div className="image-wrapper">
+              <img src={imageUrl} alt="Profile" className="profile-image" />
+              <input 
+                type="file" 
+                id="imageInput"
+                hidden="hidden" 
+                onChange={this.handleImageChange} 
+                />
+                <Tooltip title="Edit profile picture" placement="top">
+                  <IconButton onClick={this.handleEditPicture} className="button">
+                    <EditIcon color="primary" />
+                  </IconButton>
+                </Tooltip>
           </div>
           <hr />
           <div className="profile-details">
@@ -91,12 +123,47 @@ class Profile extends Component {
       {bio && <Typography variant="body2">{bio}</Typography>}
       <hr/>
       {location && (
-        <LocationOn
+        <Fragment>
+          <LocationOn color="primary" /> <span>{location}</span>
+          <hr />
+        </Fragment>
       )}
+      {website && (
+        <Fragment>
+          <LinkIcon color="primary"/>
+          <a href={website} target="_blank" rel="noopener noreferrer">
+            {' '}{website}
+          </a>
+          <hr/>
+        </Fragment>
+      )}
+      <CalendarToday color="primary"/>{' '}
+      <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
           </div>
+          <Tooltip title="Logout" placement="top">
+            <IconButton onClick={this.handleLogout}>
+              <KeyboardReturn color="primary"/>
+            </IconButton>
+          </Tooltip>
+          <EditDetails/>
         </div>
       </Paper>
-    ) : ()) : (<p>loading...</p>)
+    ) : (
+      <Paper className={classes.paper}>
+        <Typography variant="body2" align="center">
+          No profile found, please login again
+        </Typography>
+        <div className={classes.buttons}>
+          <Button variant="contained" color="primary" component={Link} to="/login">
+            Login
+          </Button>
+          <Button variant="contained" color="secondary" component={Link} to="/signup">
+            Signup
+          </Button>
+        </div>
+      </Paper>
+    )) : (<p>loading...</p>)
+
     return profileMarkup;
   }
 }
@@ -105,9 +172,13 @@ const mapStateToProps = (state) => ({
   user: state.user
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile));
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile));
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import {SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED} from '../types'
+import {SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED, LOADING_USER} from '../types'
 import axios from 'axios';
 
 export const loginUser = (userData, history) => (dispatch) => {
@@ -55,6 +55,26 @@ export const signupUser = (newUserData, history) => (dispatch) => {
       })
     });
 }
+
+export const uploadImage = (formData) => (dispatch) => {
+  dispatch({ type: LOADING_USER });
+  axios.post('/user/image', formData)
+    .then((res) => {
+      dispatch(getUserData());
+    })
+    .catch((err) => console.log(err));
+}
+
+export const editUserDetails = (userDetails) => (dispatch) => {
+  dispatch({ type: LOADING_USER });
+  axios.post('/user', userDetails)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch((err) => console.log(err));
+  
+}
+
 
 const setAuthorizationHeader = (token) => {
   const FBIdToken = `Bearer ${token}`
